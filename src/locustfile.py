d4bce35429ec1f9f442781bff228cb26f65fc5b0
Rajiv Ragavan <ragavan.r@northeastern.edu>
2025-10-05 20:07:12 -0400

    Changed task

--- src/locustfile.py
+++ src/locustfile.py
@@ -3,13 +3,9 @@ from locust import HttpUser, task, between
 class ProductUser(HttpUser):
     wait_time = between(1, 3)
 
-    @task(3)
+    @task(1)
     def get_products(self):
         self.client.get("/products")
-
-    @task(1)
-    def get_product_by_id(self):
-        self.client.get("/products/1")  # change ID as needed
 
     @task(1)
     def create_product(self):
