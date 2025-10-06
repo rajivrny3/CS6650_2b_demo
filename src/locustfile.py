3b3cc0fb1af3296dbbd999924ed497dfe394ddd3
Rajiv Ragavan <ragavan.r@northeastern.edu>
2025-10-05 20:16:24 -0400

    testing write heavy workload

--- src/locustfile.py
+++ src/locustfile.py
@@ -3,11 +3,11 @@ from locust import HttpUser, task, between
 class ProductUser(HttpUser):
     wait_time = between(1, 3)
 
-    @task(9)
+    @task(1)
     def get_products(self):
         self.client.get("/products")
 
-    @task(1)
+    @task(9)
     def create_product(self):
         self.client.post("/products", json={
             "name": "Test Product",
